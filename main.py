1428589e8d29199fbf26defbacff39203b80e1ec
Finsa-SC <finsakusumaputra@gmail.com>
2026-04-02 13:51:54 +0700

    Add Multi Processing

--- main.py
+++ main.py
@@ -6,21 +6,35 @@ root = tk.Tk()
 root.withdraw()
 
 def show_notification():
-    win = tk.Toplevel()
-    win.title("System Breach")
 
-    ms_w = 250
+    ms_w = 300
     ms_h = 100
 
     rand_x = random.randint(0, root.winfo_screenwidth() - ms_w)
     rand_y = random.randint(0, root.winfo_screenheight() - ms_h)
 
+    win = tk.Toplevel()
+    win.withdraw()
+    win.title("Hacked")
+    win.update_idletasks()
     win.geometry(f"{ms_w}x{ms_h}+{rand_x}+{rand_y}")
+    win.deiconify()
+    win.resizable(False, False)
 
-    label = tk.Label(win, text="Un-Authorization", fg="red")
-    label.pack(expand=True)
+    frame = tk.Frame(win)
+    frame.pack(expand=True, fill="both", padx=25, pady=10)
 
-    root.after(200, show_notification)
+    icon = tk.PhotoImage(file="assets/error.png")
+    icon = icon.subsample(6, 6)
+
+    icon_label = tk.Label(frame, image=icon)
+    icon_label.image = icon
+    icon_label.pack(side="left")
+
+    text_label = tk.Label(frame, text="Haha bitch, your pc has been hacked", anchor="w", justify="left")
+    text_label.pack(expand=True)
+
+    root.after(1, show_notification)
 
 if __name__ == '__main__':
     show_notification()
